6ed13b46b847d0380ce909e790ece8759f93f66a
Arif <pcarifaydogan@gmail.com>
2022-11-01 11:45:26 +0300

    added dependencies in kafka-server and pom configured

--- kafka-server/src/main/java/tr/com/trackago/kafkaserver/KafkaServerApplication.java
+++ kafka-server/src/main/java/tr/com/trackago/kafkaserver/KafkaServerApplication.java
@@ -2,8 +2,12 @@ package tr.com.trackago.kafkaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
+@ComponentScan(basePackages = {"tr.com.trackago"})
 public class KafkaServerApplication {
 
   public static void main(String[] args) {
